8b6c417402583785648eaf30b49b16500d853a87
Carlos Mertens <carlos.e.mertens.m@gmail.com>
2018-12-26 22:24:51 +0100

    style: Initiate train_model.py

---
+++ train_model.py
@@ -0,0 +1,13 @@
+# PROGRAMMER: Carlos Mertens
+# DATE CREATED: (DD/MM/YY) - 26/12/18
+# REVISED DATE: (DD/MM/YY) - Not revise it yet
+# PURPOSE: To employ several supervised algorithms to accurately model individuals' income 
+#           using data collected from the 1994 U.S. Census. Choose the best candidate 
+#           algorithm from preliminary results and optimize this algorithm to best model the 
+#           data. Goal with this implementation is to construct a model that accurately predicts 
+#           whether an individual makes more than $50,000.
+#
+# USAGE: This script requires Numpy, Pandas and Scikit_Learn to be installed within the Python environment. 
+# 
+#   Example call:
+#    python train_model.py
