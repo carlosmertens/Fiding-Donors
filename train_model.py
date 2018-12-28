cd44cd95c2520a78d0e371b25ee08f70f6b2be26
Carlos Mertens <carlos.e.mertens.m@gmail.com>
2018-12-28 14:10:50 +0100

    feat: Create function to load and the data

--- train_model.py
+++ train_model.py
@@ -11,3 +11,46 @@
 # 
 #   Example call:
 #    python train_model.py
+
+# Imports
+import numpy as np
+import pandas as pd
+
+
+def get_data(path):
+    """Load csv file.
+    
+    Function to load csv file with the 1994 U.S. Census data using Pandas. 
+    -----------
+    Parameters:
+     path - Full path to the csv file with the dataset
+    Returns:
+     data - Full dataframe
+     prices - Column with datapoints to be the targets
+     features - Dataframe without the target column to be the features.
+    """
+
+    # Load the data with Pandas
+    data = pd.read_csv(path)
+    # Call function to load data
+
+    # Display data
+    print("*** Load data for training ***\nView first 5 rows:")
+    print(data.head())
+
+    # Compute some percentage and print them
+    n_greater_50k = len(data.groupby('income').get_group('>50K'))
+    n_at_most_50k = len(data.groupby('income').get_group('<=50K'))
+    greater_percent = round((float(n_greater_50k) / float(len(data)) * 100), 2)
+
+    print("*** Explore data ***")
+    print("Total number of datapoints: {}".format(len(data)))
+    print("Individuals making more than $50,000: {}".format(n_greater_50k))
+    print("Individuals making at most $50,000: {}".format(n_at_most_50k))
+    print("Percentage of individuals making more than $50,000: {}%".format(greater_percent))
+
+    return data
+
+
+# Call function to load data
+data = get_data('data/census.csv')
